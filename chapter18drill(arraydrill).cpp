3d77ec61b53205fa430fc180302fd9aaf9845cc2
onyinyemuogilim <99292507+onyinyemuogilim@users.noreply.github.com>
2022-05-06 01:28:31 +0200

    Update chapter18drill(arraydrill).cpp

--- chapter18drill(arraydrill).cpp
+++ chapter18drill(arraydrill).cpp
@@ -1,50 +1,37 @@
 #include <iostream>
 #include <stdexcept>
+// global array
+int ga[10] ={1,2,4,8,16,32,64,128,256,512};
 
-// 1 Define a global int array
-int ga[10] = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512 };
-
-void f(int ai[], int n)
+void f(int a[], int n)
 {
-    int la[10] = { };
-    for (int i = 0; i < n; ++i) la[i] = ai[i];
+	// define local array
+	int la[10];
+	//copying values
+	for (int i = 0;i < 10;i++)
+		la[i] = ga[i];
+	//printing values
+	for (int i = 0;i < 10;i++)
+		std::cout<<la[i]<<" ";
 
-    std::cout << "Local copy\n";
-    for (const auto& a : la)
-        std::cout << a << ' ';
-    std::cout << '\n';
-
-    int* p = new int[n];
-    for (int i = 0; i < n; ++i) p[i] = ai[i];
-
-    std::cout << "Pointer to free store\n";
-    for (int i = 0; i < n; ++i)
-        std::cout << p[i] << ' ';
-    std::cout << '\n';
-
-    delete[] p;
+	// pointer
+	int* p = new int[n];
+	// copy argumented arr
+	for (int i = 0;i < 10;i++)
+		p[i] = a[i];
+	// printing
+	std::cout << std::endl;
+	for (int i = 0;i < 10;i++)
+		std::cout << p[i] << " ";
+	std::cout << std::endl;
+	delete[] p;
 }
-
-int fac(int n) { return n > 1 ? n*fac(n-1) : 1; }
-
 int main()
-try {
-    // code
-    std::cout << "Binary array\n";
-    f(ga, 10);
+{
+	f(ga, 10);
+	int aa[10] = {1,2,6,24,120,720,5040,40320,362880,3628800};
+	f(aa, 10);
 
-    int aa[10] = {};
-    for (int i = 0; i < 10; ++i)
-        aa[i] = fac(i + 1);
-
-    std::cout << "Factorial array\n";
-    f(aa, 10);
-}
-catch(std::exception& e) {
-    std::cerr << "Exception: " << e.what() << '\n';
-    return 1;
-}
-catch(...) {
-    std::cerr << "Unknown exception\n";
-    return 2;
+	
+	return 0;
 }
